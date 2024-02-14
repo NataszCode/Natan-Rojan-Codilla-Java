55b54d9feb2a60dbd320e2b2d84abc61b103251c
Natan Rojan <natan.rojan@interia.pl>
2024-02-14 21:49:49 +0100

    4.6

---
+++ kalkulator/src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static double add(double x, double y) {
+        return x + y;
+    }
+
+
+
+
+
+
+
+
+    public static void main(String[] args) {
+        System.out.println(Main.add(5, 2 ));
+    }
+}
