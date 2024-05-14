862f0a54d980b587856e366f78a705077217fd01
Natan Rojan <natan.rojan@interia.pl>
2024-05-14 21:01:33 +0200

    7.2

---
+++ kodilla-stream/src/main/java/com/kodilla/stream/immutable/ForumUserRekord.java
@@ -0,0 +1,3 @@
+package com.kodilla.stream.immutable;
+
+public record ForumUserRekord(String username, String realName) {}
--- kodilla-stream/src/main/java/com/kodilla/stream/immutable/Main.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/immutable/Main.java
@@ -1,10 +1,11 @@
 package com.kodilla.stream.immutable;
 
-public record ForumUserRecord(String username, String realName) {}
+
 
 public class Main {
     public static void main(String[] args) {
-        ForumUserRecord user = new ForumUserRecord("Netorak", "Natan Rojan");
+        ForumUserRekord user = new ForumUserRekord("Netorak", "Natan Rojan");
+        ForumUser user2 = new ForumUser("Netorak", "Natan Rojan");
         System.out.println("Username: " + user.username());
         System.out.println("Real name: " + user.realName());
     }
