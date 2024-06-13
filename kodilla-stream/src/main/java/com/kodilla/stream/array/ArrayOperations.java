2cc1cfd2dea992d49e1c80bb6f8ec622ffc3e3cd
Natan Rojan <natan.rojan@interia.pl>
2024-06-13 22:40:05 +0200

    7.5

--- kodilla-stream/src/main/java/com/kodilla/stream/array/ArrayOperations.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/array/ArrayOperations.java
@@ -1,10 +1,23 @@
 package com.kodilla.stream.array;
 
+import java.util.Arrays;
 import java.util.stream.IntStream;
 
 public interface ArrayOperations {
     static double getAverage(int[] numbers) {
         // Wyświetlanie elementów
+        if (numbers == null || numbers.length == 0) {
+            return 0;
+        }
+
+        int[] negativeValues = IntStream.range(0, numbers.length)
+                         .map(i -> numbers[i])
+                        .filter(i -> i <= 0)
+                                .toArray();
+        if (negativeValues.length > 0) {
+            throw new IllegalArgumentException("negative values not supported: " + Arrays.stream(negativeValues).mapToObj(String::valueOf).reduce("", (a, b) -> a + b));
+        }
+
         IntStream.range(0, numbers.length)
                 .map(i -> numbers[i])
                 .forEach(System.out::println);
--- kodilla-stream/src/test/java/com/kodilla/stream/array/ArrayOperationsTestSuite.java
+++ kodilla-stream/src/test/java/com/kodilla/stream/array/ArrayOperationsTestSuite.java
@@ -1,7 +1,11 @@
 package com.kodilla.stream.array;
 
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.parallel.Execution;
+
+import java.util.stream.IntStream;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class ArrayOperationsTestSuite {
 
@@ -16,4 +20,18 @@ public class ArrayOperationsTestSuite {
 
         assertEquals(10.5, average, 0.01);
     }
+    @Test
+
+    void testCases() {
+
+        int[] numbers = {-3,5};
+        IllegalArgumentException thrown = assertThrows(
+                IllegalArgumentException.class,
+                () -> ArrayOperations.getAverage(numbers),
+                "Expected doThing() to throw, but it didn't"
+        );
+        String exceptionMessage = thrown.getMessage();
+        assertEquals("negative values not supported: -3", exceptionMessage);
+    }
+
 }
