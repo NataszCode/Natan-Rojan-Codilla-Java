5e47ca0796b6e58e67a78d646aeaf941eb071a84
Natan Rojan <natan.rojan@interia.pl>
2024-06-28 17:11:49 +0200

    13.1

---
+++ kodilla-exception/src/test/java/com/kodilla/spring/forum/ForumUserTestSuite.java
@@ -0,0 +1,23 @@
+package com.kodilla.spring.forum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+public class ForumUserTestSuite {
+
+    @Autowired
+    private ForumUser forumUser;
+
+    @Test
+    void testGetUsername() {
+
+        String username = forumUser.getUsername();
+
+
+        assertEquals("John Smith", username);
+    }
+}
