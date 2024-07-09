33f42c20b8fd8ff12cda4802a3b91806de4cf1b9
Natan Rojan <natan.rojan@interia.pl>
2024-07-09 21:12:46 +0200

    14.5

--- kodilla-singleton/src/main/java/com/kodilla/patterns/builder/bigmac/Bigmac.java
+++ kodilla-singleton/src/main/java/com/kodilla/patterns/builder/bigmac/Bigmac.java
@@ -31,4 +31,45 @@ public final class Bigmac {
     public List<String> getIngredients() {
         return new ArrayList<>(ingredients);
     }
+
+    public static class BigmacBuilder {
+        private String bun;
+        private int burgers;
+        private String sauce;
+        private List<String> ingredients = new ArrayList<>();
+
+        public BigmacBuilder bun(String bun) {
+            this.bun = bun;
+            return this;
+        }
+
+        public BigmacBuilder burgers(int burgers) {
+            this.burgers = burgers;
+            return this;
+        }
+
+        public BigmacBuilder sauce(String sauce) {
+            this.sauce = sauce;
+            return this;
+        }
+
+        public BigmacBuilder ingredient(String ingredient) {
+            ingredients.add(ingredient);
+            return this;
+        }
+
+        public Bigmac build() {
+            return new Bigmac(this.bun, this.burgers, this.sauce, this.ingredients);
+        }
+
+        @Override
+        public String toString() {
+            return "Bigmac{" +
+                    "bun='" + bun + '\'' +
+                    ", burgers=" + burgers +
+                    ", sauce='" + sauce + '\'' +
+                    ", ingredients=" + ingredients +
+                    '}';
+        }
+    }
 }
